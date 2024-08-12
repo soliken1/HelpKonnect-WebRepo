7d680b91c16b3eb739a897af8ab6108e3dda46dc
Soliken <kennethrex456@gmail.com>
2024-08-12 22:34:19 +0800

    Initial Commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <label className="text-red-300 text-2xl font-bold">
+        Help-Konnect's Website
+      </label>
+    </div>
+  );
+}
