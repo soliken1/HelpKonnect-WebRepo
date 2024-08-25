68ed92746efb3c36b312fd0c0bf46c7bcb0948c8
Soliken <kennethrex456@gmail.com>
2024-08-25 14:42:13 +0800

    Modal View For Adding Professionals

---
+++ src/components/professionals/AddProfModal.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+
+export default function Modal({ isOpen, onClose, children }) {
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    if (isOpen) {
+      setTimeout(() => setShowModal(true), 50);
+    } else {
+      setShowModal(false);
+    }
+  }, [isOpen]);
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+      <div
+        className={`bg-white p-10 rounded-lg shadow-md shadow-red-200 relative w-2/3 h-3/4 transition-transform duration-200 ease-in-out ${
+          showModal ? "scale-100" : "scale-0"
+        }`}
+      >
+        <button
+          className="absolute top-6 right-6 text-gray-500 text-3xl hover:text-gray-700"
+          onClick={onClose}
+        >
+          &times;
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+}
--- src/components/professionals/Table.js
+++ src/components/professionals/Table.js
@@ -1,6 +1,18 @@
+"use client";
 import React from "react";
+import { useState } from "react";
+import Modal from "./AddProfModal";
 
 function Table() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  const handleAddProffssional = async (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div className="w-full flex flex-col p-10">
       <label className="text-lg font-bold">Professionals</label>
@@ -8,10 +20,87 @@ function Table() {
         Professional List on Sample Facility
       </label>
       <div className="flex justify-end mt-10 mb-4">
-        <button className="bg-green-400 ps-10 pe-10 pt-2 pb-2 text-white font-semibold rounded-xl shadow-md shadow-gray-400">
+        <button
+          onClick={openModal}
+          className="bg-green-400 ps-10 pe-10 pt-2 pb-2 text-white font-semibold rounded-xl shadow-md shadow-gray-400"
+        >
           Add Professional
         </button>
       </div>
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        <h2 className="text-2xl font-semibold mb-1">Add Professional</h2>
+        <label className="text-gray-400 font-semibold">
+          Add Pending Professionals on the Applying on Your Facility
+        </label>
+        <div className="h-5/6 flex flex-row w-full mt-4">
+          <div className="w-96 h-full flex justify-center items-center">
+            <img
+              src="/SampleProfile.jpg"
+              className="h-80 w-80 rounded-full shadow-md"
+            />
+          </div>
+          <div className="w-2/3 bg-red-200 p-10 rounded-md">
+            <form onSubmit={handleAddProffssional}>
+              <div className="relative mt-5">
+                <input
+                  type="text"
+                  name="text"
+                  id="floating_outlined"
+                  className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-black rounded-lg border-1 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-red-300 focus:outline-none focus:ring-0 focus:border-red-300 peer"
+                  placeholder=" "
+                  autoComplete="off"
+                />
+                <label
+                  htmlFor="floating_outlined"
+                  className="absolute text-sm font-semibold text-red-300 dark:text-red-300 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] bg-transparent px-2 peer-focus:px-2 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-100 peer-focus:-translate-y-8 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+                >
+                  Username
+                </label>
+              </div>
+              <div className="relative mt-7">
+                <input
+                  type="text"
+                  name="text"
+                  id="floating_outlined"
+                  className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-black rounded-lg border-1 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-red-300 focus:outline-none focus:ring-0 focus:border-red-300 pointer-events-none opacity-50 peer"
+                  placeholder=" "
+                  autoComplete="off"
+                />
+                <label
+                  htmlFor="floating_outlined"
+                  className="absolute text-sm font-semibold text-red-300 dark:text-red-300 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] bg-transparent px-2 peer-focus:px-2 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-100 peer-focus:-translate-y-8 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+                >
+                  License
+                </label>
+              </div>
+              <div className="relative mt-7">
+                <input
+                  type="text"
+                  name="text"
+                  id="floating_outlined"
+                  className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-black rounded-lg border-1 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-red-300 focus:outline-none focus:ring-0 focus:border-red-300 pointer-events-none opacity-50 peer"
+                  placeholder=" "
+                  autoComplete="off"
+                />
+                <label
+                  htmlFor="floating_outlined"
+                  className="absolute text-sm font-semibold text-red-300 dark:text-red-300 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] bg-transparent px-2 peer-focus:px-2 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-100 peer-focus:-translate-y-8 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+                >
+                  Gender
+                </label>
+              </div>
+              <div className="w-full h-32 flex items-center justify-center mt-7">
+                <button
+                  type="submit"
+                  className="bg-green-400 ps-10 pe-10 pt-2 pb-2 text-white font-semibold rounded-xl shadow-md shadow-gray-400"
+                >
+                  Add Professional
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </Modal>
       <div className="overflow-x-auto shadow-sm shadow-red-50">
         <table className="min-w-full">
           <thead>
