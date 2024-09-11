f547f6ac883f42ff00409e1e18b6b8079f3611c1
Soliken <kennethrex456@gmail.com>
2024-09-12 00:06:54 +0800

    Update Formula for Chart Active Users by Weeks

--- src/utils/dau.js
+++ src/utils/dau.js
@@ -10,7 +10,10 @@ import {
 import { db } from "@/configs/firebaseConfigs";
 export async function getDailyActiveUsers() {
   const now = Timestamp.now();
-  const yesterday = new Timestamp(now.seconds - 24 * 60 * 60, now.nanoseconds);
+  const yesterday = new Timestamp(
+    now.seconds - 24 * 60 * 60 * 7,
+    now.nanoseconds
+  );
 
   const q = query(
     collection(db, "userActivity"),
@@ -20,7 +23,7 @@ export async function getDailyActiveUsers() {
   const querySnapshot = await getDocs(q);
   const data = querySnapshot.docs.map((doc) => doc.data());
 
-  console.log("getDailyActiveUsers data:", data); // Add this line to check data
+  console.log("getDailyActiveUsers data:", data);
 
   return data;
 }
