e1500d77fdae199adfbb9f0e0216819d902149f6
Soliken <kennethrex456@gmail.com>
2024-09-22 00:24:06 +0800

    Unrestricting Public Folder Images Test #1

--- src/middleware.js
+++ src/middleware.js
@@ -2,26 +2,29 @@ import { NextResponse } from "next/server";
 
 export function middleware(request) {
   const currentUser = request.cookies.get("currentUser")?.value;
-
   const { pathname } = request.nextUrl;
 
-  // If user is authenticated and trying to access the login page, redirect to dashboard
+  if (
+    pathname.startsWith("/_next") ||
+    pathname.startsWith("/static") ||
+    pathname.startsWith("/images") ||
+    pathname.includes("Icons") ||
+    pathname.includes("Logo")
+  ) {
+    return NextResponse.next();
+  }
+
   if (currentUser && pathname === "/") {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
-  // If user is NOT authenticated and trying to access any other page except login, redirect to login
   if (!currentUser && pathname !== "/") {
-    return NextResponse.redirect(new URL("/", request.url)); // Redirect to login page
+    return NextResponse.redirect(new URL("/", request.url));
   }
 
-  // Allow access to requested page
   return NextResponse.next();
 }
 
 export const config = {
-  // Match all routes except the login page ("/")
-  matcher: [
-    "/((?!api|_next|static|favicon.ico).*)", // Matches all routes except static, API, and other Next.js internal routes
-  ],
+  matcher: ["/((?!api|_next|static|favicon.ico).*)"],
 };
