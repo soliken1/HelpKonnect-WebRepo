36068a4fdfb8105618ce453f2d6f99c2dedfc74a
Soliken <kennethrex456@gmail.com>
2024-10-08 18:50:58 +0800

    Rapid API Key

--- src/pages/api/rapidAPIKey.js
+++ src/pages/api/rapidAPIKey.js
@@ -1,12 +1,12 @@
 export default function handler(req, res) {
   if (req.method === "GET") {
-    const gtranslateKey = process.env.GTRANSLATE_API_KEY;
+    const rapidAPIKey = process.env.RAPID_API_KEY;
 
-    if (!gtranslateKey) {
+    if (!rapidAPIKey) {
       return res.status(500).json({ error: "API key not found" });
     }
 
-    res.status(200).json({ gtranslateKey });
+    res.status(200).json({ rapidAPIKey });
   } else {
     res.status(405).json({ error: "Method not allowed" });
   }
