365cada7e884a20b31ceeb1446f59e522c051155
Soliken <kennethrex456@gmail.com>
2024-10-27 22:29:38 +0800

    Added Search User on Messages

--- src/components/messages/UserList.js
+++ src/components/messages/UserList.js
@@ -1,26 +1,41 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
 function List({ users, currentUser }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <div className="w-full flex flex-col p-10">
+    <div className="w-full flex flex-row gap-5 p-10">
       <div className="flex flex-col md:w-4/12 md:h-full p-4 rounded-lg shadow-md">
         <label className="text-black font-bold text-xl">Chats</label>
         <input
           type="text"
           className="w-full py-2 px-4 rounded-full placeholder:text-sm mt-4 text-sm shadow-md"
           placeholder="Search..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
         <div className="w-full h-full flex flex-col py-4 overflow-auto">
           {users
-            .filter((user) => user.id !== currentUser)
+            .filter(
+              (user) =>
+                user.id !== currentUser &&
+                ((user.facilityName &&
+                  user.facilityName
+                    .toLowerCase()
+                    .includes(searchTerm.toLowerCase())) ||
+                  (user.username &&
+                    user.username
+                      .toLowerCase()
+                      .includes(searchTerm.toLowerCase())))
+            )
             .map((user) => (
               <Link
                 href={`/message/${user.id}`}
                 key={user.id}
-                className="flex flex-row gap-2 h-16 items-center cursor-pointer hover:bg-gray-300 px-4 duration-100 rounded-md"
+                className="flex flex-row gap-2 py-2 h-16 items-center cursor-pointer hover:bg-gray-300 px-4 duration-100 rounded-md"
               >
                 <Image
                   src={user.imageUrl ? user.imageUrl : "/UserIcon.svg"}
@@ -40,6 +55,9 @@ function List({ users, currentUser }) {
               </Link>
             ))}
         </div>
+      </div>
+      <div className="w-full h-full flex justify-center items-center shadow-md text-gray-500 text-lg">
+        Select A User To Chat or Interact With!
       </div>
     </div>
   );
