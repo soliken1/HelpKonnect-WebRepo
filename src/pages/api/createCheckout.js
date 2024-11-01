2e06c4176b956d35380027eee23add69a0ecc37d
Soliken <kennethrex456@gmail.com>
2024-11-01 19:51:54 +0800

    Update Request of Checkout Route

--- src/pages/api/createCheckout.js
+++ src/pages/api/createCheckout.js
@@ -2,26 +2,43 @@ import axios from "axios";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    const { amount } = req.body;
-
+    const { amount, username, email, phone } = req.body;
+    const total = amount * 100;
     try {
       const response = await axios.post(
         "https://api.paymongo.com/v1/checkout_sessions",
         {
-          data: {
-            amount,
-            currency: "PHP",
-            payment_method_types: ["card"],
-            success_url: "https://helpkonnect.vercel.app/status/success",
-            cancel_url: "https://helpkonnect.vercel.app/status/failed",
-          },
-        },
-        {
+          accept: "application/json",
           headers: {
             Authorization: `Basic ${Buffer.from(
               `${process.env.PAYMONGO_SECRET_KEY}:`
             ).toString("base64")}`,
             "Content-Type": "application/json",
+          },
+        },
+        {
+          data: {
+            data: {
+              attributes: {
+                billing: { name: username, email: email, phone: phone },
+              },
+              send_email_receipt: true,
+              show_description: true,
+              show_line_items: true,
+              payment_method_types: ["gcash"],
+              line_items: [
+                {
+                  currency: "PHP",
+                  amount: total,
+                  description: "test",
+                  name: "test",
+                  quantity: 1,
+                },
+              ],
+              description: "test",
+              success_url: "https://helpkonnect.vercel.app/status/success",
+              cancel_url: "https://helpkonnect.vercel.app/status/failed",
+            },
           },
         }
       );
