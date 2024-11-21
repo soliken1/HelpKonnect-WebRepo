d8e5904ae212461b19ff25a05d36699b714b50ef
Soliken <155744710+soliken1@users.noreply.github.com>
2024-11-21 19:02:10 +0800

    Increase User Privilege

--- src/pages/api/createUser.js
+++ src/pages/api/createUser.js
@@ -13,6 +13,7 @@ export default async function handler(req, res) {
       {
         id: userId,
         name: username,
+        role: "admin",
       },
     ]);
     res.status(200).json({ message: "User created successfully" });
